5e651e6c6b0a9ab2be0de3e3674489ff20d95ba0
soumaya mejri <soumaya mejri@DESKTOP-9TR48SP.timsoft.lan>
2020-01-01 22:20:39 +0100

    section4-activity

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { AppareilService } from '../appareil.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
   styleUrls: ['./appareil.component.css']
 })
-export class AppareilComponent {
+export class AppareilComponent implements OnInit {
 
   @Input() appareilName: string;
   @Input() appareilStatus: string;
+  @Input() index: number;
+  isAuth;
 
-  constructor() { }
+  constructor(private appareilServce: AppareilService, private authService: AuthService) { }
+  ngOnInit(){
+    this.isAuth = this.authService.isAuth;  
+  }
+  
 
   getStatus() {
     return this.appareilStatus;
@@ -22,6 +29,14 @@ export class AppareilComponent {
       return 'green';
     } else if(this.appareilStatus === 'éteint') {
       return 'red';
+    }
+  }
+  onSwitch(){
+    if(this.appareilStatus==='allumé') {
+      this.appareilServce.switchOffOne(this.index);
+    }
+    else if (this.appareilStatus==='éteint') {
+      this.appareilServce.switchOnOne(this.index);
     }
   }
 
